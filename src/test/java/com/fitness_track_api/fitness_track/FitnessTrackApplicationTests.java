fa56be6bd1fb03ba2c3551060c2499390c0dd528
NipunaDassanayake <nipuna920@gmail.com>
2025-04-10 16:24:00 +0530

    initial commit

---
+++ src/test/java/com/fitness_track_api/fitness_track/FitnessTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitness_track_api.fitness_track;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
