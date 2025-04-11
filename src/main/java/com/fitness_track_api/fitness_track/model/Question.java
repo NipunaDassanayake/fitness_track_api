bdb18e77c43242e6db35beffe5fa83ff6cb9551b
Chinthana Weligamage <chinthanaweligamage1@gmail.com>
2025-04-11 13:12:02 +0530

    Create Models for Questions and Answers

---
+++ src/main/java/com/fitness_track_api/fitness_track/model/Question.java
@@ -0,0 +1,34 @@
+//package com.fit_track_api.fit_track_api.model;
+//import jakarta.persistence.*;
+//import lombok.Data;
+//import java.time.LocalDateTime;
+//import java.util.ArrayList;
+//import java.util.List;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+//@Getter
+//@Setter
+//@Entity
+//
+//public class Question {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Column(nullable = false)
+//    private String title;
+//
+//    @Column(nullable = false, length = 2000)
+//    private String content;
+//
+//    private LocalDateTime createdAt = LocalDateTime.now();
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id", nullable = false)
+//    private User user;
+//
+//    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL, orphanRemoval = true)
+//    @OrderBy("createdAt ASC")
+//    private List<Answer> answers = new ArrayList<>();
+//}
---
+++ src/main/java/com/fitness_track_api/fitness_track/model/Questionnaire.java
@@ -0,0 +1,25 @@
+package com.fit_track_api.fit_track_api.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity
+public class Questionnaire {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String questionText;
+
+    @ManyToOne
+    @JoinColumn(name = "workout_plan_id")
+    private WorkoutPlan workoutPlan;
+
+    @OneToMany(mappedBy = "questionnaire")
+    private List<UserAnswer> userAnswers;
+
+
+}
+
