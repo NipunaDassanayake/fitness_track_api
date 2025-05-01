744a44629214057a0e99cda00b12203dd5d478e2
Shakya Pinnawala <shakyapinnawala@gmail.com>
2025-05-01 11:20:20 +0530

    did some updaets in the model

--- src/main/java/com/fitness_track_api/fitness_track/model/Achievement.java
+++ src/main/java/com/fitness_track_api/fitness_track/model/Achievement.java
@@ -1,10 +1,14 @@
-package com.fitness_track_api.fitness_track.model;
+package com.fit_track_api.fit_track_api.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,6 +27,9 @@ public class Achievement {
     @Column(length = 1000)
     private String description;
 
+    @ElementCollection
+    private List<String> imageUrl;
+
     private LocalDate achievedDate = LocalDate.now();
 
 //    private String templateType;
@@ -35,4 +42,22 @@ public class Achievement {
     @JoinColumn(name = "workout_plan_id")
     @JsonManagedReference
     private WorkoutPlan workoutPlan;
+
+    @Column(nullable = false)
+    private int likedCount = 0;
+
+    @OneToMany(mappedBy = "achievement")
+    @JsonBackReference
+    private List<Comment> comments = new ArrayList<>();
+
+    @Column(name = "video_url")
+    private String videoUrl;
+
+    @ManyToMany
+    @JoinTable(
+            name = "achievement_likes",
+            joinColumns = @JoinColumn(name = "achievement_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private List<User> likedBy = new ArrayList<>();
 }
