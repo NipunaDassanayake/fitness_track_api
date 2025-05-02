3955671ed4832aa6eca81a5f2df79b543e9077b4
Shakya Pinnawala <shakyapinnawala@gmail.com>
2025-05-02 11:25:10 +0530

    update in repo

--- src/main/java/com/fitness_track_api/fitness_track/repository/CommentRepository.java
+++ src/main/java/com/fitness_track_api/fitness_track/repository/CommentRepository.java
@@ -1,14 +1,11 @@
-package com.fitness_track_api.fitness_track.repository;
-
+package com.fit_track_api.fit_track_api.repository;
 import com.fit_track_api.fit_track_api.model.Comment;
 import com.fit_track_api.fit_track_api.model.WorkoutPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByPost(WorkoutPost post);
-    List<Comment> findByPostOrderByCreatedAtAsc(WorkoutPost post);
+    List<Comment> findByAchievementId(Long achievementId);
 }
