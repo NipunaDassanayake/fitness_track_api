4d00d7dcf036059557018ed9684cdbd704463ffe
NipunaDassanayake <nipuna920@gmail.com>
2025-05-03 11:29:16 +0530

    dto updated

--- src/main/java/com/fit_track_api/fit_track_api/controller/dto/request/CreateExerciseRequestDTO.java
+++ src/main/java/com/fit_track_api/fit_track_api/controller/dto/request/CreateExerciseRequestDTO.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class CreateExerciseRequestDTO {
@@ -16,4 +17,6 @@ public class CreateExerciseRequestDTO {
 
     @NotNull(message = "Order is required")
     private Integer order;
+
+    private MultipartFile image; // For a single image
 }
