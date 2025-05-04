c2fee6253b0cc14175fd232b4b8c33c3638f9cf4
Shakya Pinnawala <shakyapinnawala@gmail.com>
2025-05-04 12:17:11 +0530

    update achivevemt controller

--- src/main/java/com/fitness_track_api/fitness_track/controller/AchievementController.java
+++ src/main/java/com/fitness_track_api/fitness_track/controller/AchievementController.java
@@ -8,6 +8,7 @@ import com.fit_track_api.fit_track_api.model.Achievement;
 import com.fit_track_api.fit_track_api.service.AchievementService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,37 +21,28 @@ public class AchievementController {
 
     private final AchievementService achievementService;
 
-    @PostMapping("/share-achievement/{workoutPlanId}")
+    @PostMapping(value = "/share-achievement/{workoutPlanId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<String> shareAsAchievement(
             @RequestParam Long userId,
             @PathVariable Long workoutPlanId,
-            @RequestBody CreateAchievementDTO createAchievementDTO) {
-        Achievement achievement = achievementService.shareAchievement(
-                userId, workoutPlanId, createAchievementDTO);
-        return ResponseEntity.ok("User Share a Achievement");
+            @ModelAttribute CreateAchievementDTO createAchievementDTO) {
+        try {
+            Achievement achievement = achievementService.shareAchievement(userId, workoutPlanId, createAchievementDTO);
+            return ResponseEntity.ok("User shared an achievement");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body("Error: " + e.getMessage());
+        }
     }
+
 
     @PutMapping("/{achievementId}")
     public ResponseEntity<String> updateAchievement(
             @PathVariable Long achievementId,
-            @RequestBody UpdateAchievementDTO updateAchievementDTO) {
-        System.out.println(updateAchievementDTO.getDescription());
+            @ModelAttribute UpdateAchievementDTO updateAchievementDTO) {
         Achievement updatedAchievement = achievementService.updateAchievement(achievementId, updateAchievementDTO);
         return ResponseEntity.ok("Achievement updated successfully ");
     }
 
-    @DeleteMapping("/users/{userId}/achievements/{achievementId}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteUserAchievement(
-            @PathVariable Long userId,
-            @PathVariable Long achievementId) {
-        achievementService.deleteAchievement(userId, achievementId);
-    }
 
-    @GetMapping("/{achievementId}")
-    public ResponseEntity<AchievementResponseDTO> getAchievementById(
-            @PathVariable Long achievementId) {
-        AchievementResponseDTO achievement = achievementService.getAchievementById(achievementId);
-        return ResponseEntity.ok(achievement);
-    }
+
 }
