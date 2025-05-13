dcf495b8bd915295b27d89e267af1173b0190c47
Shakya Pinnawala <shakyapinnawala@gmail.com>
2025-05-13 11:10:10 +0530

    comment service created

---
+++ src/main/java/com/fitness_track_api/fitness_track/service/impl/CommentServiceImpl.java
@@ -0,0 +1,43 @@
+package com.fitness_track_api.fitness_track.service.impl;
+
+import com.fit_track_api.fit_track_api.controller.dto.request.CreateCommentRequestDTO;
+import com.fit_track_api.fit_track_api.controller.dto.response.GetCommentResponseDTO;
+import com.fit_track_api.fit_track_api.model.Achievement;
+import com.fit_track_api.fit_track_api.model.Comment;
+import com.fit_track_api.fit_track_api.model.User;
+import com.fit_track_api.fit_track_api.repository.AchievementRepository;
+import com.fit_track_api.fit_track_api.repository.CommentRepository;
+import com.fit_track_api.fit_track_api.repository.UserRepository;
+import com.fit_track_api.fit_track_api.service.CommentService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class CommentServiceImpl implements CommentService {
+
+    private CommentRepository commentRepository;
+    private AchievementRepository achievementRepository;
+    private UserRepository userRepository;
+
+    @Override
+    public Comment addComment(Long achievementId, Long userId, CreateCommentRequestDTO dto) {
+        Achievement achievement = achievementRepository.findById(achievementId)
+                .orElseThrow(() -> new RuntimeException("Post not found"));
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        Comment comment = new Comment();
+        comment.setContent(dto.getContent());
+        comment.setAchievement(achievement);
+        comment.setUser(user);
+
+        return commentRepository.save(comment);
+    }
+
+
+
+}
